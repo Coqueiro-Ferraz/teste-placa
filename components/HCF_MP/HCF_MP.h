c0ff4af88bf4d22be86de60ac65026c627e4a22a
Hagaceef <121056899+Coqueiro-Ferraz@users.noreply.github.com>
2025-04-03 12:25:20 -0300

    Update HCF_MP.h

--- components/HCF_MP/HCF_MP.h
+++ components/HCF_MP/HCF_MP.h
@@ -3,9 +3,9 @@
    // #include "esp_err.h"
  
 
-    void iniciar_MP(void);
-    void rotacionar_MP(int horario, float graus);
+    void iniciar_driver(void);
+    void rotacionar_driver(int horario, float graus);
 
-    void iniciar_driver(int Fim_de_curso_inicial, int Fim_de_curso_final);
-    void rotacionar_driver(int abrir, float graus, uint8_t saidas_atual);
+    void iniciar_MP(int Fim_de_curso_inicial, int Fim_de_curso_final);
+    void rotacionar_MP(int abrir, float graus, uint8_t saidas_atual);
 #endif
